43cc69e36fbdc753232dc2b38850558618e305b0
gusti rifqi <dewalaut66@icloud.com>
2026-05-12 16:38:25 +0700

    step 20

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class buku 
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+
+        return *this;   // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+
+    return 0;
+}
