f5295aa40520f589bd38f09ab7492a8ab74e45da
gusti rifqi <dewalaut66@icloud.com>
2026-05-12 10:15:27 +0700

    file staticfunction(f)

---
+++ staticfunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string> //menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+
+    private:
+         static int nim;
+    public:
+         int id;
+         string nama;
+    
+    void setID();
+    void printAll();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+static int getNim() {
+        return nim;
+    }
+}
