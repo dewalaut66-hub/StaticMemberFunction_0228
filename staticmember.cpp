0718cf2400cfe688b10875811aa9c90307669bc0
gusti rifqi <dewalaut66@icloud.com>
2026-05-12 10:05:57 +0700

    step 4

---
+++ staticmember.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa { //membuat classmahasiswa
+    
+pupblic:
+    static int nim; //deklarasi veriabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
+//deklarasi prosedur setID() dan printAll()
+void setID();
+void printAll();
+
+//pembuatan constructor Mahasiswa dengan parameter pnama
+Mahasiswa(string pnama) :nama(pnama) {
+    setID();
+    }
+};
+
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 10;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID(){
+    id = ++nim;
+};
+
+void Mahasiswa::printAll(){
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
+
+int main() {
+    //membuat object dan memberi nilai
+    
+}
